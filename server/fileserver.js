ea3666c8ed3f722d63944dd04cb87efa974b49dc
lizs <lizs4ever@163.com>
2015-12-26 14:19:10 +0800

    node 文件服务器

---
+++ server/fileserver.js
@@ -0,0 +1,71 @@
+var static = require('node-static');
+var chokidar = require('chokidar');
+var md5 = require('md5');
+var fs = require('fs');
+var dispatch = require('./dispatcher.js');
+var http = require('http');
+var log = console.log.bind(console);
+var fileServer = new static.Server('./public');
+
+var md5json = {};
+
+var server = http.createServer(
+	dispatch(
+	{
+        '/md5': function(req, res)
+        {
+            res.end(JSON.stringify(md5json));
+        },
+
+        '/(\\w+.\\w+)': function(req, res)
+        {            
+		    req.addListener('end', function ()
+		    {
+		        fileServer.serve(req, res, function (err, result)
+		        {
+		            if (err)
+		            { // There was an error serving the file
+		                console.error("Error serving " + req.url + " - " + err.message);
+
+		                // Respond to the client
+		                res.writeHead(err.status, err.headers);
+		                res.end();
+		            }
+		        });
+		    }).resume();
+        }
+    }));
+
+server.listen(8080);
+
+
+// One-liner for current directory, ignores .dotfiles
+var watcher = chokidar.watch('./public', {
+  ignored: /[\/\\]\./,
+  persistent: true
+});
+
+var getmd5 = function(path, cb){
+	fs.readFile(path, function(err, buf) {
+  		cb(err ? '' : md5(buf), err);
+
+  		log("md5json prints below:")
+	  	for (var i in md5json) {
+		    if (!md5json.hasOwnProperty(i)) continue; // safety!
+		    log(md5json[i])
+		}
+	});
+}
+
+watcher
+  .on('add', path => getmd5(path, (code, err)=> md5json[path] = code))
+  .on('change', path => getmd5(path, (code, err)=> md5json[path] = code))
+  .on('unlink', path => 
+	  {
+	  	delete md5json[path];
+	  	log(path, ' removed');
+	  })
+  .on('error', error => log(`Watcher error: ${error}`))
+  .on('ready', () => {
+	  	log('Initial scan complete. Ready for changes');
+	});
